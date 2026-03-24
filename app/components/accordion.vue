24cae471db89ca00677d85d012a607adac77f3fd
navi9000 <icdi7434@gmail.com>
2026-03-24 12:30:56 +0900

    accordion

---
+++ app/components/accordion.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+const isOpen = ref(false)
+</script>
+
+<template>
+  <div class="accordion">
+    <div class="headingcontainer">
+      <div class="heading">
+        <slot name="heading" />
+      </div>
+      <div class="button" @click="isOpen = !isOpen">
+        <slot name="button" />
+      </div>
+    </div>
+    <Transition name="inlaywrapper">
+      <div v-if="isOpen" class="inlay">
+        <slot name="answer" />
+      </div>
+    </Transition>
+  </div>
+</template>
+
+<style scoped>
+.accordion {
+  color: white;
+}
+
+.headingcontainer {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  cursor: pointer;
+}
+
+.inlaywrapper-enter-active,
+.inlaywrapper-leave-active {
+  transition: 300ms;
+  height: 200px;
+}
+
+.inlaywrapper-enter-from,
+.inlaywrapper-leave-to {
+  height: 0;
+  overflow-y: scroll;
+  transition: height 0.5s;
+}
+
+.inlay {
+  overflow: hidden;
+}
+</style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,7 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Accordion from "~/components/accordion.vue"
+</script>
 
 <template>
-  <div></div>
+  <div>Index</div>
 </template>
